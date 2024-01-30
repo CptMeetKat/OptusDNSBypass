2901b58ce13af314b0146a1833bde0f94c421975
Reece <reece.cares@gmail.com>
2024-01-30 20:02:37 +1100

    Refactored app entry

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ConfigManager = require('./ConfigManager.js');
 const NetworkMonitor = require('./NetworkMonitor/NetworkMonitor.js');
 
 
-async function run()
+function parseArgs()
 {
    const parser = new ArgumentParser({ description: 'Use Optus Sagemcom 5393 API.' })
    parser.add_argument('-m', '--monitor', { dest: 'monitor', action: 'store_const',
@@ -20,25 +20,34 @@ async function run()
    let md_group = parser.add_argument_group('Modify DNS', 'Modify the current DNS');
    md_group.add_argument('-md', '--modifyDNS', {metavar: ['ip1','ip2'], type:'str', help:'DNS IP addresses', nargs:2});
    
-   let xargs = parser.parse_args()
-   
-
-   let config = ConfigManager.getConfig("./settings.conf");
-   const args = process.argv.slice(2);
-   if(xargs.viewDNS)
-      new DNSBypass(config).run();
-   else if(xargs.modifyDNS)
-   {
-      console.log("ModifyingDNS");
-      config.dns1 = xargs.modifyDNS[0]; //need to validate IPs?
-      config.dns2 = xargs.modifyDNS[1];
-      config.modifyDNS = true; //these need to be removed from the config, restructure DNSBypass object?
-      new DNSBypass(config).run();
-   }
-   else if(xargs.monitor)
-      new NetworkMonitor(config).run();
-   else 
-      console.log("**No action specified**");
+   return parser.parse_args();
 }
 
-run();
+
+async function run(args)
+{
+   let config = ConfigManager.getConfig("./settings.conf");
+   if(args.viewDNS)
+      await new DNSBypass(config).run();
+   else if(args.modifyDNS)
+   {
+      console.log("ModifyingDNS");
+      config.dns1 = args.modifyDNS[0]; //need to validate IPs?
+      config.dns2 = args.modifyDNS[1];
+      config.modifyDNS = true; //these need to be removed from the config, restructure DNSBypass object?
+      await new DNSBypass(config).run();
+   }
+   else if(args.monitor)
+      await new NetworkMonitor(config).run();
+   else 
+      console.log("**No action specified**");
+
+}
+
+async function main()
+{
+   let args = parseArgs();
+   await run(args);
+}
+
+main();
